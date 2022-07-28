4517acf8dadbb07d21478566573cd4b98a3f8c35
Sayak Mallick <sayakmallickkv@gmail.com>
2022-07-28 11:28:10 +0530

    Initial Commit

---
+++ Core Python/2.Data_Types.py
@@ -0,0 +1,30 @@
+y = 10
+print(y, type(y))
+
+price = 26.52
+print(price, type(price))
+
+com = 3+4j
+print(com, type(com))
+
+str = "Sayak Mallick"
+print(str, type(str))
+
+data = [10,20,30, "S", "Mallick"]
+print(data, type(data))
+
+tdata = (10,20,30, "S", "Mallick")
+print(data, type(tdata))
+
+
+sdata = {10,20,10,20,'S',"S","Mallick"}
+print(sdata, type(sdata))
+
+ddata = {100:'Sayak',101:'Priyanka',102:'Souvik'}
+print(ddata, type(ddata))
+
+rg = range(5)
+rg1 = range(0,20,2)
+print(rg, type(rg))
+print(rg1, type(rg1))
+
